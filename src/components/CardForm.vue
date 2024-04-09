0f0eafebdcdb726354355db40ce9918dfcbccb46
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-04-09 11:18:34 -0300

    create Form and DigitalCard inital code

---
+++ src/components/CardForm.vue
@@ -0,0 +1,99 @@
+<script setup>
+defineProps({
+  title: {
+    required: true,
+    type: String
+  },
+  subtitle: {
+    required: true,
+    type: String
+  },
+  phone: {
+    required: true,
+    type: String
+  },
+  address: {
+    required: true,
+    type: String
+  },
+  email: {
+    required: true,
+    type: String
+  }
+})
+
+defineEmits([
+  'change:image',
+  'update:title',
+  'update:subtitle',
+  'update:phone',
+  'update:address',
+  'update:email'
+])
+</script>
+
+<template>
+  <form>
+    <div class="input-field">
+      <label for="logo">Imagem:</label>
+      <input type="file" id="logo" @change="$emit('change:image', $event.target.files[0])" />
+    </div>
+
+    <div class="input-field">
+      <label for="card-title">Título:</label>
+      <input
+        type="text"
+        id="card-title"
+        placeholder="Informe o tiulo do cartão"
+        :value="title"
+        @input="$emit('update:title', $event.target.value)"
+      />
+    </div>
+
+    <div class="input-field">
+      <label for="card-subtitle">Sub-título:</label>
+      <input
+        type="text"
+        id="card-subtitle"
+        placeholder="Informe o tiulo do cartão"
+        :value="subtitle"
+        @input="$emit('update:subtitle', $event.target.value)"
+      />
+    </div>
+
+    <div class="input-field">
+      <label for="phone">Telefone:</label>
+      <input
+        type="tel"
+        id="phone"
+        placeholder="(xx) xxxx-xxxx..."
+        :value="phone"
+        @input="$emit('update:phone', $event.target.value)"
+      />
+    </div>
+
+    <div class="input-field">
+      <label for="address">Endereço:</label>
+      <input
+        type="text"
+        id="address"
+        placeholder="Rua. Exemplo 000, Bairro"
+        :value="address"
+        @input="$emit('update:address', $event.target.value)"
+      />
+    </div>
+
+    <div class="input-field">
+      <label for="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        placeholder="dominio@exemplo.com"
+        :value="email"
+        @input="$emit('update:email', $event.target.value)"
+      />
+    </div>
+
+    <input type="submit" value="Export to PNG" />
+  </form>
+</template>
---
+++ src/components/DigitalCard.vue
@@ -0,0 +1,69 @@
+<script setup>
+defineProps({
+  cardData: {
+    image: {
+      required: true,
+      type: String
+    },
+    title: {
+      required: true,
+      type: String
+    },
+    subtitle: {
+      required: true,
+      type: String
+    },
+    phone: {
+      required: true,
+      type: String
+    },
+    address: {
+      required: true,
+      type: String
+    },
+    email: {
+      required: true,
+      type: String
+    }
+  },
+  image: {
+    required: true,
+    type: [String, null]
+  }
+})
+</script>
+
+<template>
+  <div id="digital-card">
+    <img :src="image ?? '/default-image.jpg'" alt="logo" />
+
+    <div id="info">
+      <h2>{{ cardData.title || 'Título' }}</h2>
+      <h3>{{ cardData.subtitle || 'Subtítulo' }}</h3>
+
+      <ul>
+        <li>Telefone: {{ cardData.phone }}</li>
+        <li>Endereço: {{ cardData.address }}</li>
+        <li>Email: {{ cardData.email }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#digital-card {
+  display: flex;
+  justify-content: space-between;
+  padding: 40px;
+  border-radius: 14px;
+  width: 764px;
+  background-color: #fff;
+  color: #000;
+}
+
+#digital-card img {
+  width: 224px;
+  height: 224px;
+  border-radius: 50%;
+}
+</style>
